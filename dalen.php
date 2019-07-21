ce0c12ae13e95226bdcebd8e840777ed080835a9
Thiago Senna <trsenna@gmail.com>
2019-07-21 13:08:01 -0300

    ready for 0.2.0

--- dalen.php
+++ dalen.php
@@ -5,7 +5,7 @@
  * Author:      Thiago Senna
  * Author URI:  http://thremes.com.br
  * Description: The Dalen WordPress framework that helps make modern PHP projects possible.
- * Version:     0.1.0
+ * Version:     0.2.0
  *
  * @package   Dalen
  * @author    Thiago Senna <thiago@thremes.com.br>
